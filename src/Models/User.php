4b557007a33a9264dfbc1dfcc68c330080e79996
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-04-15 18:24:08 +0200

    Init User model

---
+++ src/Models/User.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Backstage\UserManagement\Models;
+
+use Backstage\UserManagement\Concerns;
+use Backstage\UserManagement\Concerns\HasBackstageManagement;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
+use Illuminate\Contracts\Auth\CanResetPassword;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\Contracts\HasApiTokens as ContractsHasApiTokens;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable implements CanResetPassword, ContractsHasApiTokens, FilamentUser, MustVerifyEmail
+{
+    use CanResetPassword;
+    use Concerns\Conditionals\HasConditionals;
+
+    use Concerns\Relations\HasRelations;
+    use Concerns\Scopes\HasScopes;
+    // Sanctum
+    use HasApiTokens;
+    use HasBackstageManagement;
+    /** @use HasFactory<\Database\Factories\UserFactory> */
+    use HasFactory;
+
+    // Spatie roles
+    use HasRoles;
+
+    use MustVerifyEmail;
+    use Notifiable;
+
+    // Illuminate contracts:
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+}
