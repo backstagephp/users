5492e0baed27d8f3e2d35053fa910923c21503d3
arduinomaster22 <91618246+arduinomaster22@users.noreply.github.com>
2025-05-14 09:07:23 +0000

    Fix styling

--- src/Pages/Auth/EditProfile.php
+++ src/Pages/Auth/EditProfile.php
@@ -2,10 +2,10 @@
 
 namespace Backstage\Filament\Users\Pages\Auth;
 
-use Filament\Pages\Auth\EditProfile as BaseEditProfile;
-use Filament\Forms\Components\Select;
 use Backstage\Filament\Users\Models\User;
 use Backstage\Laravel\Users\Enums\NotificationType;
+use Filament\Forms\Components\Select;
+use Filament\Pages\Auth\EditProfile as BaseEditProfile;
 
 class EditProfile extends BaseEditProfile
 {
@@ -36,21 +36,21 @@ class EditProfile extends BaseEditProfile
         $options = [];
 
         foreach ($types as $type) {
-            $options[$type->value] =  $type->label();
+            $options[$type->value] = $type->label();
         }
 
         return Select::make('notification_preferences')
             ->label(__('Notification preferences'))
-            ->options(fn() => $options)
+            ->options(fn () => $options)
             ->live()
-            ->placeholder(fn() => ('Select notification preferences'))
+            ->placeholder(fn () => ('Select notification preferences'))
             ->searchingMessage(__('Searching notification types...'))
             ->searchPrompt(__('Search notification types...'))
             ->saveRelationshipsUsing(function (User $record, array $state) {
-                $state  = collect($state)->map(fn($value) => NotificationType::from($value));
+                $state = collect($state)->map(fn ($value) => NotificationType::from($value));
 
                 $state->each(function (NotificationType $type) use ($record) {
-                    if (!$record->notificationPreferences->contains('navigation_type', $type->value)) {
+                    if (! $record->notificationPreferences->contains('navigation_type', $type->value)) {
 
                         $record->notificationPreferences()->create([
                             'navigation_type' => $type->value,
@@ -70,7 +70,7 @@ class EditProfile extends BaseEditProfile
         $user = $this->getUser();
 
         if ($user->notificationPreferences->isNotEmpty()) {
-            $data['notification_preferences'] = $user->notificationPreferences->pluck('navigation_type')->map(fn(NotificationType $record) => $record->value)->toArray();
+            $data['notification_preferences'] = $user->notificationPreferences->pluck('navigation_type')->map(fn (NotificationType $record) => $record->value)->toArray();
         }
 
         return $data;
