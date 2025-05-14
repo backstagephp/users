76540a2391ae8e388d7a940624d1cf780e71d211
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-05-14 11:04:17 +0200

    wip

---
+++ src/Pages/Auth/EditProfile.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Backstage\Users\Pages\Auth;
+
+use Filament\Pages\Auth\EditProfile;
+use Filament\Forms\Components\Select;
+use Backstage\Filament\Users\Models\User;
+use Backstage\Laravel\Users\Enums\NotificationType;
+
+class Profile extends EditProfile
+{
+    protected function getForms(): array
+    {
+        return [
+            'form' => $this->form(
+                $this->makeForm()
+                    ->schema([
+                        $this->getNameFormComponent(),
+                        $this->getEmailFormComponent(),
+                        static::getNotificationFormComponent(),
+                        $this->getPasswordFormComponent(),
+                        $this->getPasswordConfirmationFormComponent(),
+                    ])
+                    ->operation('edit')
+                    ->model($this->getUser())
+                    ->statePath('data')
+                    ->inlineLabel(! static::isSimple()),
+            ),
+        ];
+    }
+
+    public static function getNotificationFormComponent(): Select
+    {
+        $types = NotificationType::cases();
+
+        $options = [];
+
+        foreach ($types as $type) {
+            $options[$type->value] =  $type->label();
+        }
+
+        return Select::make('notification_preferences')
+            ->label(__('Notification preferences'))
+            ->options(fn() => $options)
+            ->live()
+            ->placeholder(fn() => ('Select notification preferences'))
+            ->searchingMessage(__('Searching notification types...'))
+            ->searchPrompt(__('Search notification types...'))
+            ->saveRelationshipsUsing(function (User $record, array $state) {
+                $state  = collect($state)->map(fn($value) => NotificationType::from($value));
+
+                $state->each(function (NotificationType $type) use ($record) {
+                    if (!$record->notificationPreferences->contains('navigation_type', $type->value)) {
+
+                        $record->notificationPreferences()->create([
+                            'navigation_type' => $type->value,
+                        ]);
+                    }
+                });
+
+                $record->notificationPreferences()->whereNotIn('navigation_type', $state)->delete();
+            })
+            ->multiple();
+    }
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $data = parent::mutateFormDataBeforeFill($data);
+
+        $user = $this->getUser();
+
+        if ($user->notificationPreferences->isNotEmpty()) {
+            $data['notification_preferences'] = $user->notificationPreferences->pluck('navigation_type')->map(fn(NotificationType $record) => $record->value)->toArray();
+        }
+
+        return $data;
+    }
+}
